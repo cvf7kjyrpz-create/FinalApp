a056d574d20eade15dbd167d9e649b4db17e2720
cvf7kjyrpz-create <cvf7kjyrpz@privaterelay.appleid.com>
2025-11-22 18:21:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,25 @@
-const CACHE_NAME = "lagerpro-v4-cache-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-512.png"
-];
-
-// Installation → Dateien in Cache legen
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
-// Aktivierung → alte Caches löschen
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
+  event.waitUntil(clients.claim());
 });
 
-// Bei Abruf → erst Cache, dann Netzwerk
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(
-      (response) => response || fetch(event.request)
-    )
+    caches.open("lagerpro-cache").then((cache) => {
+      return cache.match(event.request).then((resp) => {
+        return (
+          resp ||
+          fetch(event.request).then((response) => {
+            if (event.request.url.startsWith(self.location.origin)) {
+              cache.put(event.request, response.clone());
+            }
+            return response;
+          })
+        );
+      });
+    })
   );
 });
